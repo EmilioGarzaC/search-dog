a1be24d7c13df6678bc2e04f48835569807ac820
Emilio Garza <116039130+EmilioGarzaC@users.noreply.github.com>
2023-06-09 16:44:31 -0600

    Creacion de archivos y folders base para el proyecto
    * Archivos y carpetas de estructura

---
+++ modules/htmlManager.py
@@ -0,0 +1,12 @@
+class htmlManager:
+    def __init__(self):
+        pass
+    
+    
+
+    def readHtmlFile(self, fileName):
+        pass 
+    
+    
+    def removeHtmlTags(self, htmlContent):
+        pass 
