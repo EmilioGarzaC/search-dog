337a79213c8611ea2fb644fd0a9a3e051c193ce6
Emilio Garza <116039130+EmilioGarzaC@users.noreply.github.com>
2023-06-09 20:41:25 -0600

    Consolidated functions for first phase (#3)
    * Archivos y carpetas de estructura
    
    * Added placeholder log for folder
    
    * Consolidated, refactored, and tested functions from team

--- modules/_executor.py
+++ modules/_executor.py
@@ -1,22 +1,76 @@
-import os 
+import os
+import time
 from modules.htmlManager import htmlManager
-from modules.txtManager import txtManager
+from modules.textManager import textManager
 
 class Executor:
     def __init__(self):
-        self.htmlManager = htmlManager()
-        self.txtManager = txtManager()
+        self.htmlMgr = htmlManager()
+        self.txtMgr = textManager()
         pass
     
     
 
     def readHtmlFolder(self, folderPath):
-        # Lista de archivos en folder
-        htmlFiles = os.listdir(folderPath)
-        print(htmlFiles)
+        htmlData = {}
+        fileList = os.listdir(folderPath)
         
-        # Se ejecuta por cada archivo:
-        #   self.htmlManager.readHtmlFile(fileName)
-        
-        pass
+        for fileName in fileList:
+            fileData = self.htmlMgr.readHtmlFile(f"{folderPath}/{fileName}")
+            htmlData[fileData['fileName']] = {'content':fileData['fileContents'], 'readTime':fileData['readTime']}
+            
+        return htmlData
     
+    
+    def removeHtmlTags(self, htmlData):
+        htmlFileContentsNoTags = {}
+        for fileName, fileData in htmlData.items():
+            content = fileData['content']
+
+            dataNoTags = self.htmlMgr.removeHtmlTags(content)
+
+            htmlFileContentsNoTags[fileName] = {
+                'content': fileData['content'],
+                'contentNoTags': dataNoTags['contentNoTags'],
+                'readTime': fileData['readTime'],
+                'removalTime': dataNoTags['removalTime']
+            }
+
+        return htmlFileContentsNoTags
+           
+    
+    def splitContentIntoOrderedList(self, htmlData):
+        htmlFileContentsSplit= {}
+        for fileName, fileData in htmlData.items():
+            contentToSplit = fileData['contentNoTags']
+            
+            contentSplitData = self.txtMgr.splitText(contentToSplit)
+            cleanWordsList = self.txtMgr.filterTrash(contentSplitData['splitContentList'])
+            contentSplitOrderedData = self.txtMgr.orderList(cleanWordsList)
+            
+            htmlFileContentsSplit[fileName] = {
+                'content': fileData['content'],
+                'contentNoTags': fileData['contentNoTags'],
+                'contentSplit': contentSplitData['splitContentList'],
+                'contentSplitOrdered': contentSplitOrderedData['orderedList'],
+                'readTime': fileData['readTime'],
+                'removalTime': fileData['removalTime'],
+                'splitTime': contentSplitData['splitTime'],
+                'orderTime': contentSplitOrderedData['orderTime']
+            }
+            
+        return htmlFileContentsSplit
+    
+    
+    def writeLog(self, fileName, title, body, footer):
+        logText = f"############# {title} #############\n{body}\n{footer}"
+        self.txtMgr.writeToTxt(path='log-files', fileName=fileName, text=logText)
+    
+    
+    def writeToTxt(self, path, fileName, content):
+        self.txtMgr.writeToTxt(path=f'output-files/{path}', fileName=fileName, text=content)
+    
+        
+    def purgeNonWords(self, listToCleanse):
+        cleanWordsList = self.txtMgr.filterTrash(listToCleanse)
+        return cleanWordsList
